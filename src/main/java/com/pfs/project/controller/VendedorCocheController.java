2363d3fcefc2f292069189ee4f3a46404224d7cf
nicolas.pons@pfsgroup.es <nicolas.pons@pfsgroup.es@nicolas-pons-hp>
2020-02-07 10:24:07 +0100

    VendedorCoche controller JSONObject

--- src/main/java/com/pfs/project/controller/VendedorCocheController.java
+++ src/main/java/com/pfs/project/controller/VendedorCocheController.java
@@ -2,13 +2,13 @@ package com.pfs.project.controller;
 
 import java.util.List;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.pfs.project.model.Coche;
 import com.pfs.project.model.Vendedor;
@@ -28,40 +28,46 @@ public class VendedorCocheController {
 	
 	@RequestMapping(value="/list", method=RequestMethod.GET)
 	public @ResponseBody CustomResponse listCochesVendedor(@PathVariable Integer idVendedor) {
+		CustomResponse response = new CustomResponse();
 		Vendedor v = vendedorService.getVendedor(idVendedor);
 		List<Coche> coches = vendedorService.getCochesVendedor(v);
+		response.setMsg("Coches del vendedor "+ v.getUsuario());
+		JSONObject array = new JSONObject();
+		array.put("coches", coches);
+		response.setBody(array);
 		return null;
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ModelAndView detailCocheVendedor(@PathVariable Integer idVendedor, @PathVariable Integer id) {
-		ModelAndView modelAndView = new ModelAndView("coche-page");
+	public @ResponseBody CustomResponse detailCocheVendedor(@PathVariable Integer idVendedor, @PathVariable Integer id) {
+		CustomResponse response = new CustomResponse();
 		Coche c = cocheService.getCoche(id);
-		//TODO tratar coche
-		return modelAndView;
+		response.setMsg("Correcto");
+		response.setBody(c.toJSON());
+		return response;
 	}
 	
 	@RequestMapping(value="/{id}/sell", method=RequestMethod.PUT)
-	public ModelAndView sellCocheVendedor(@PathVariable Integer idVendedor, @PathVariable Integer id) {
-		ModelAndView modelAndView = new ModelAndView("coche-page");
+	public @ResponseBody CustomResponse sellCocheVendedor(@PathVariable Integer idVendedor, @PathVariable Integer id) {
+		CustomResponse response = new CustomResponse();
 		Coche c = cocheService.getCoche(id);
 		Vendedor v = vendedorService.getVendedor(idVendedor);
 		c.setVendedor(v);
 		cocheService.updateCoche(c);
-		//TODO Mostrar alerta
-		System.out.println(c.print());
-		return modelAndView;
+		response.setMsg("Coche "+c.getMatricula()+" añadido al vendedor "+v.getUsuario());
+		response.setBody(c.toJSON());
+		return response;
 	}
 	
 	@RequestMapping(value="/{id}/refund", method=RequestMethod.PUT)
-	public ModelAndView refundCocheVendedor(@PathVariable Integer idVendedor, @PathVariable Integer cocheId) {
-		ModelAndView modelAndView = new ModelAndView("coche-page");
+	public @ResponseBody CustomResponse refundCocheVendedor(@PathVariable Integer cocheId) {
+		CustomResponse response = new CustomResponse();
 		Coche c = cocheService.getCoche(cocheId);
 		Vendedor v = vendedorService.getVendedor(1);
 		c.setVendedor(v);
 		cocheService.updateCoche(c);
-		//TODO Mostrar alerta
-		System.out.println(c.print());
-		return modelAndView;
+		response.setMsg("Coche "+c.getMatricula()+" eliminado al vendedor "+v.getUsuario());
+		response.setBody(c.toJSON());
+		return response;
 	}
 }
